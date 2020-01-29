94827c526b2f6890e11531069ece0d81cbd440e5
Evan Darwin <evan@relta.net>
2020-01-28 17:38:33 -0800

    Require PHP >=7.2, clean up code
    Also updated Travis CI config to use new PHP version, bye bye 5.x!

--- src/JSendBuilder.php
+++ src/JSendBuilder.php
@@ -2,177 +2,168 @@
 
 namespace EvanDarwin\JSend;
 
-class JSendBuilder
-{
-  private $status  = "success";
-  private $data    = array();
-  private $errors  = array();
-  private $message = '';
-  private $code    = null;
+use InvalidArgumentException;
 
-  /**
-   * Construct a new JSendBuilder object.
-   *
-   * @param string      $status  The status of the response
-   * @param array       $data    The data to include in the response
-   * @param string|null $message The human-readable message to include with the response
-   * @param int|null    $code    The human (error) code
-   */
-  public function __construct(
-      $status = JSendResponse::STATUS_SUCCESS,
-      array $data = array(),
-      array $errors = array(),
-      $message = null,
-      $code = null)
-  {
-    $this->status  = $status;
-    $this->data    = $data;
-    $this->errors  = $errors;
-    $this->message = $message;
-    $this->code    = $code;
-  }
-
-  /**
-   * Sets the status to successful.
-   */
-  public function success()
-  {
-    $this->status = JSendResponse::STATUS_SUCCESS;
-
-    return $this;
-  }
-
-  /**
-   * Sets the status to be an error.
-   */
-  public function error()
-  {
-    $this->status = JSendResponse::STATUS_ERROR;
-
-    return $this;
-  }
-
-  /**
-   * Alias for ::failed()
-   *
-   * @deprecated
-   */
-  public function fail()
-  {
-    $this->failed();
-  }
-
-  /**
-   * Sets the status to failed.
-   */
-  public function failed()
-  {
-    $this->status = JSendResponse::STATUS_FAIL;
-
-    return $this;
-  }
-
-  /**
-   * Sets the status of the response. Valid options are:
-   *  - 'success'
-   *  - 'error'
-   *  - 'fail'
-   *  - Any constant in JSendResponse
-   *
-   * @param $status
-   *
-   * @return $this
-   *
-   * @deprecated
-   */
-  public function status($status)
-  {
-    if (is_string($status)) {
-      switch ($status) {
-        case 'success':
-          $this->status = JSendResponse::STATUS_SUCCESS;
-          break;
-        case 'error':
-          $this->status = JSendResponse::STATUS_ERROR;
-          break;
-        case 'fail':
-          $this->status = JSendResponse::STATUS_FAIL;
-          break;
-        default:
-          throw new \InvalidArgumentException("Unable to parse status '${status}'");
-      }
-    } else {
-      $this->status = $status;
+class JSendBuilder {
+    private $status;
+    private $data;
+    private $errors;
+    private $message;
+    private $code;
+    
+    /**
+     * Construct a new JSendBuilder object.
+     *
+     * @param int         $status  The status of the response
+     * @param array       $data    The data to include in the response
+     * @param array       $errors
+     * @param string|null $message The human-readable message to include with the response
+     * @param int|null    $code    The human (error) code
+     */
+    public function __construct(
+        $status = JSendResponse::STATUS_SUCCESS,
+        array $data = array(),
+        array $errors = array(),
+        $message = null,
+        $code = null) {
+        $this->status = $status;
+        $this->data = $data;
+        $this->errors = $errors;
+        $this->message = $message;
+        $this->code = $code;
     }
-
-    return $this;
-  }
-
-  /**
-   * Set the data of the response
-   *
-   * @param array $data
-   *
-   * @return $this
-   */
-  public function data(array $data)
-  {
-    $this->data = $data;
-
-    return $this;
-  }
-
-  /**
-   * Sets the errors in the response
-   *
-   * @param array $errors The errors
-   *
-   * @return $this
-   */
-  public function errors(array $errors)
-  {
-    $this->errors = $errors;
-
-    return $this;
-  }
-
-  /**
-   * Set the human readable message
-   *
-   * @param $message
-   *
-   * @return $this
-   */
-  public function message($message)
-  {
-    $this->message = $message;
-
-    return $this;
-  }
-
-  /**
-   * Set the error code
-   *
-   * @param $code
-   *
-   * @return $this
-   */
-  public function code($code)
-  {
-    $this->code = $code;
-
-    return $this;
-  }
-
-  /**
-   * Returns the built object
-   *
-   * @return \EvanDarwin\JSend\JSendResponse
-   *
-   * @throws \InvalidArgumentException
-   */
-  public function get()
-  {
-    return new JSendResponse($this->status, $this->data, $this->errors, $this->code, $this->message);
-  }
+    
+    /**
+     * Sets the status to successful.
+     */
+    public function success() {
+        $this->status = JSendResponse::STATUS_SUCCESS;
+        
+        return $this;
+    }
+    
+    /**
+     * Sets the status to be an error.
+     */
+    public function error() {
+        $this->status = JSendResponse::STATUS_ERROR;
+        
+        return $this;
+    }
+    
+    /**
+     * Alias for ::failed()
+     *
+     * @deprecated
+     */
+    public function fail() {
+        $this->failed();
+    }
+    
+    /**
+     * Sets the status to failed.
+     */
+    public function failed() {
+        $this->status = JSendResponse::STATUS_FAIL;
+        
+        return $this;
+    }
+    
+    /**
+     * Sets the status of the response. Valid options are:
+     *  - 'success'
+     *  - 'error'
+     *  - 'fail'
+     *  - Any constant in JSendResponse
+     *
+     * @param $status
+     *
+     * @return $this
+     *
+     * @deprecated
+     */
+    public function status($status) {
+        if (is_string($status)) {
+            switch ($status) {
+                case 'success':
+                    $this->status = JSendResponse::STATUS_SUCCESS;
+                    break;
+                case 'error':
+                    $this->status = JSendResponse::STATUS_ERROR;
+                    break;
+                case 'fail':
+                    $this->status = JSendResponse::STATUS_FAIL;
+                    break;
+                default:
+                    throw new InvalidArgumentException("Unable to parse status '${status}'");
+            }
+        } else {
+            $this->status = $status;
+        }
+        
+        return $this;
+    }
+    
+    /**
+     * Set the data of the response
+     *
+     * @param array $data
+     *
+     * @return $this
+     */
+    public function data(array $data) {
+        $this->data = $data;
+        
+        return $this;
+    }
+    
+    /**
+     * Sets the errors in the response
+     *
+     * @param array $errors The errors
+     *
+     * @return $this
+     */
+    public function errors(array $errors) {
+        $this->errors = $errors;
+        
+        return $this;
+    }
+    
+    /**
+     * Set the human readable message
+     *
+     * @param $message
+     *
+     * @return $this
+     */
+    public function message($message) {
+        $this->message = $message;
+        
+        return $this;
+    }
+    
+    /**
+     * Set the error code
+     *
+     * @param $code
+     *
+     * @return $this
+     */
+    public function code($code) {
+        $this->code = $code;
+        
+        return $this;
+    }
+    
+    /**
+     * Returns the built object
+     *
+     * @return JSendResponse
+     *
+     * @throws InvalidArgumentException
+     */
+    public function get() {
+        return new JSendResponse($this->status, $this->data, $this->errors, $this->code, $this->message);
+    }
 }
--- src/JSendResponse.php
+++ src/JSendResponse.php
@@ -2,152 +2,146 @@
 
 namespace EvanDarwin\JSend;
 
-class JSendResponse
-{
-  const STATUS_SUCCESS = 1;
-  const STATUS_FAIL    = 2;
-  const STATUS_ERROR   = 3;
+use InvalidArgumentException;
 
-  private $status;
-  private $data           = null;
-  private $errors         = array();
-  private $code, $message = null;
-
-  public function __construct($status, $data = null, $errors = null, $code = null, $message = null)
-  {
-    $validStatuses = array(
-        self::STATUS_SUCCESS,
-        self::STATUS_FAIL,
-        self::STATUS_ERROR
-    );
-
-    // Validate they gave us a valid status.
-    if (!in_array($status, $validStatuses) || $status == null) {
-      throw new \InvalidArgumentException("Invalid status type provided. Use one of the JSendResponse constants");
+class JSendResponse {
+    public const STATUS_SUCCESS = 1;
+    public const STATUS_FAIL    = 2;
+    public const STATUS_ERROR   = 3;
+    
+    private $status;
+    private $data;
+    private $errors;
+    private $code, $message;
+    
+    public function __construct($status, $data = null, $errors = null, $code = null, $message = null) {
+        $validStatuses = array(
+            self::STATUS_SUCCESS,
+            self::STATUS_FAIL,
+            self::STATUS_ERROR
+        );
+        
+        // Validate they gave us a valid status.
+        if ($status === null || !in_array($status, $validStatuses, true)) {
+            throw new InvalidArgumentException('Invalid status type provided. Use one of the JSendResponse constants');
+        }
+        
+        // Validate $data
+        if ($data !== null && !is_array($data)) {
+            throw new InvalidArgumentException('Invalid data type provided, must be array or null');
+        }
+        
+        // Validate $errors
+        if (!is_array($errors)) {
+            throw new InvalidArgumentException('Invalid errors type provided, must be array');
+        }
+        
+        // Validate $code
+        if (!is_int($code) && $code !== null && !is_string($code)) {
+            throw new InvalidArgumentException('Status code must be null, an integer, or a string');
+        }
+        
+        // Validate $message
+        if ($message !== null && !is_string($message)) {
+            throw new InvalidArgumentException('Error message must be null or a string');
+        }
+        
+        $this->status = $status;
+        $this->data = $data;
+        $this->errors = $errors;
+        $this->code = $code;
+        $this->message = $message;
     }
-
-    // Validate $data
-    if (!is_null($data) && !is_array($data)) {
-      throw new \InvalidArgumentException("Invalid data type provided, must be array or null");
+    
+    /**
+     * Returns the data contained in the response
+     *
+     * @return null|array
+     */
+    public function getData(): ?array {
+        return $this->data;
     }
-
-    // Validate $errors
-    if (!is_array($errors)) {
-      throw new \InvalidArgumentException("Invalid errors type provided, must be array");
+    
+    /**
+     * Returns the errors contained in the response
+     *
+     * @return array
+     */
+    public function getErrors(): array {
+        return $this->errors;
     }
-
-    // Validate $code
-    if (!is_int($code) && !is_string($code) && !is_null($code)) {
-      throw new \InvalidArgumentException("Status code must be null, an integer, or a string");
+    
+    /**
+     * Returns the reference code for this error.
+     *
+     * @return int
+     */
+    public function getCode(): int {
+        return $this->code;
     }
-
-    // Validate $message
-    if (!is_null($message) && !is_string($message)) {
-      throw new \InvalidArgumentException("Error message must be null or a string");
+    
+    /**
+     * @return null
+     */
+    public function getMessage() {
+        return $this->message;
     }
-
-    $this->status  = $status;
-    $this->data    = $data;
-    $this->errors  = $errors;
-    $this->code    = $code;
-    $this->message = $message;
-  }
-
-  /**
-   * Returns the data contained in the response
-   *
-   * @return null|array
-   */
-  public function getData()
-  {
-    return $this->data;
-  }
-
-  /**
-   * Returns the errors contained in the response
-   *
-   * @return array
-   */
-  public function getErrors()
-  {
-    return $this->errors;
-  }
-
-  /**
-   * Returns the reference code for this error.
-   *
-   * @return int
-   */
-  public function getCode()
-  {
-    return $this->code;
-  }
-
-  /**
-   * @return null
-   */
-  public function getMessage()
-  {
-    return $this->message;
-  }
-
-  /**
-   * @return string
-   */
-  public function __toString()
-  {
-    return $this->getResponse();
-  }
-
-  /**
-   * Returns the request response to return to the client
-   *
-   * @return string
-   */
-  public function getResponse()
-  {
-    return json_encode($this->getArray());
-  }
-
-  /**
-   * Returns the array version of the request response
-   *
-   * @return array
-   */
-  public function getArray()
-  {
-    $errors = empty($this->errors) ? null : $this->errors;
-
-    $response = array(
-        'status' => $this->getStatus(),
-        'data'   => $this->data,
-        'errors' => $errors,
-    );
-
-    if (!is_null($this->code)) {
-      $response['code'] = $this->code;
+    
+    /**
+     * @return string
+     */
+    public function __toString() {
+        return $this->getResponse();
     }
-
-    if (!is_null($this->message)) {
-      $response['message'] = $this->message;
+    
+    /**
+     * Returns the request response to return to the client
+     *
+     * @return string
+     */
+    public function getResponse(): string {
+        return json_encode($this->getArray());
     }
-
-    return $response;
-  }
-
-  /**
-   * Returns the status of the request.
-   *
-   * @return string
-   */
-  public function getStatus()
-  {
-    if ($this->status == self::STATUS_SUCCESS) {
-      return "success";
-    } else if ($this->status == self::STATUS_FAIL) {
-      return "fail";
-    } else {
-      return "error";
+    
+    /**
+     * Returns the array version of the request response
+     *
+     * @return array
+     */
+    public function getArray(): array {
+        $errors = empty($this->errors) ? null : $this->errors;
+        
+        $response = array(
+            'status' => $this->getStatus(),
+            'data'   => $this->data,
+            'errors' => $errors,
+        );
+        
+        if ($this->code !== null) {
+            $response['code'] = $this->code;
+        }
+        
+        if ($this->message !== null) {
+            $response['message'] = $this->message;
+        }
+        
+        return $response;
     }
-  }
+    
+    /**
+     * Returns the status of the request.
+     *
+     * @return string
+     */
+    public function getStatus(): string {
+        if ($this->status === self::STATUS_SUCCESS) {
+            return 'success';
+        }
+        
+        if ($this->status === self::STATUS_FAIL) {
+            return 'fail';
+        }
+        
+        return 'error';
+    }
 }
